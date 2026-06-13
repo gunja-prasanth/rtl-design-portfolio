a3894203cc5bbe8105cab98aff4910c249ddd356
Prasanth <gunjaprasanth693@gmail.com>
2026-06-13 19:43:56 +0530

    Add Verilog number representation topic

---
+++ 01_verilog/03_number_representation/examples/binary_numbers.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.06.2026 19:39:17
+// Design Name: 
+// Module Name: binary_numbers
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module binary_numbers;
+
+reg [3:0] data;
+
+initial begin 
+    data = 4'b1010;
+ end
+ 
+ endmodule
