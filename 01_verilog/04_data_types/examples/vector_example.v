e3ff451154a645b3be9d0d43cf655924bccf6208
Prasanth <gunjaprasanth693@gmail.com>
2026-06-14 21:36:49 +0530

    Add Verilog data types notes, examples, and interview preparation

---
+++ 01_verilog/04_data_types/examples/vector_example.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.06.2026 21:35:20
+// Design Name: 
+// Module Name: vector_example
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vector_example;
+
+reg  [3:0] counter;
+wire [7:0] data_bus;
+
+initial begin
+    counter = 4'b1010;
+end
+
+endmodule
